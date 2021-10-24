e6e10f7aacb525b7f4635794ebfce0fc79b34ce4
Константин Абраменко <kostaabramenko695@gmail.com>
2021-10-24 22:16:41 +0700

    first

---
+++ Prog3SemLab3/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace Prog3SemLab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.xyPointsGridView = new System.Windows.Forms.DataGridView();
+            this.xValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.yValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.launchBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.importToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.googleSheetsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.excelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.randomToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.xyPointsGridView)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // xyPointsGridView
+            // 
+            this.xyPointsGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.xyPointsGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.xValue,
+            this.yValue});
+            this.xyPointsGridView.Location = new System.Drawing.Point(12, 42);
+            this.xyPointsGridView.Name = "xyPointsGridView";
+            this.xyPointsGridView.Size = new System.Drawing.Size(193, 150);
+            this.xyPointsGridView.TabIndex = 0;
+            // 
+            // xValue
+            // 
+            this.xValue.HeaderText = "x";
+            this.xValue.Name = "xValue";
+            this.xValue.Width = 75;
+            // 
+            // yValue
+            // 
+            this.yValue.HeaderText = "y";
+            this.yValue.Name = "yValue";
+            this.yValue.Width = 75;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.launchBtn,
+            this.importToolStripMenuItem,
+            this.closeBtn});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1058, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // launchBtn
+            // 
+            this.launchBtn.Name = "launchBtn";
+            this.launchBtn.Size = new System.Drawing.Size(80, 20);
+            this.launchBtn.Text = "Рассчитать";
+            this.launchBtn.Click += new System.EventHandler(this.LaunchBtn_Click);
+            // 
+            // importToolStripMenuItem
+            // 
+            this.importToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.googleSheetsToolStripMenuItem,
+            this.excelToolStripMenuItem,
+            this.randomToolStripMenuItem});
+            this.importToolStripMenuItem.Name = "importToolStripMenuItem";
+            this.importToolStripMenuItem.Size = new System.Drawing.Size(107, 20);
+            this.importToolStripMenuItem.Text = "Импортировать";
+            // 
+            // googleSheetsToolStripMenuItem
+            // 
+            this.googleSheetsToolStripMenuItem.Name = "googleSheetsToolStripMenuItem";
+            this.googleSheetsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.googleSheetsToolStripMenuItem.Text = "Google таблицы";
+            // 
+            // excelToolStripMenuItem
+            // 
+            this.excelToolStripMenuItem.Name = "excelToolStripMenuItem";
+            this.excelToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.excelToolStripMenuItem.Text = "Excel";
+            // 
+            // randomToolStripMenuItem
+            // 
+            this.randomToolStripMenuItem.Name = "randomToolStripMenuItem";
+            this.randomToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.randomToolStripMenuItem.Text = "Рандом";
+            // 
+            // closeBtn
+            // 
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.Size = new System.Drawing.Size(54, 20);
+            this.closeBtn.Text = "Выход";
+            this.closeBtn.Click += new System.EventHandler(this.CloseBtn_Click);
+            // 
+            // chart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            this.chart.Location = new System.Drawing.Point(224, 42);
+            this.chart.Name = "chart";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.Name = "Points";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Name = "Line";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Name = "Sqr";
+            this.chart.Series.Add(series1);
+            this.chart.Series.Add(series2);
+            this.chart.Series.Add(series3);
+            this.chart.Size = new System.Drawing.Size(822, 300);
+            this.chart.TabIndex = 2;
+            this.chart.Text = "chart";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1058, 372);
+            this.Controls.Add(this.chart);
+            this.Controls.Add(this.xyPointsGridView);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.xyPointsGridView)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView xyPointsGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn xValue;
+        private System.Windows.Forms.DataGridViewTextBoxColumn yValue;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem launchBtn;
+        private System.Windows.Forms.ToolStripMenuItem importToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem googleSheetsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem excelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem randomToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeBtn;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+    }
+}
+
---
+++ Prog3SemLab3/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MathNet.Symbolics;
+using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+using MathNet.Numerics.LinearAlgebra.Double.Solvers;
+
+namespace Prog3SemLab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        async private void LaunchBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double[] xValue = new double[xyPointsGridView.RowCount];
+                double[] yValue = new double[xyPointsGridView.RowCount];
+                for (int point = 0; point < xyPointsGridView.RowCount - 1; ++point)
+                {
+                    xValue[point] = double.Parse(xyPointsGridView[0, point].Value.ToString());
+                    yValue[point] = double.Parse(xyPointsGridView[1, point].Value.ToString());
+                }
+                PointsDraw(xValue, yValue);
+                var coefsContainer = CoefFinding(xValue, yValue);
+                await Task.Run(() => LineDraw(coefsContainer, minX(xValue), maxX(xValue)));
+            }
+            catch
+            {
+
+            }
+
+        }
+
+
+
+        private double minX(double[] xValue)
+        {
+            double minX = xValue[0];
+            for (int point = 0; point < xValue.Length; ++point)
+            {
+                if (xValue[point] < minX)
+                {
+                    minX = xValue[point];
+                }
+            }
+            return minX;
+        }
+
+
+
+        private double maxX(double[] xValue)
+        {
+            double maxX = xValue[0];
+            for (int point = 0; point < xValue.Length; ++point)
+            {
+                if (xValue[point] > maxX)
+                {
+                    maxX = xValue[point];
+                }
+            }
+            return maxX;
+        }
+
+
+
+        private void PointsDraw(double[] xValue, double[] yValue)
+        {
+            chart.Series[0].Points.Clear();
+            for (int point = 0; point < xValue.Length; ++point)
+            {
+                chart.Series[0].Points.AddXY(xValue[point], yValue[point]);
+            }
+        }
+
+
+
+        private void LineDraw(List<Vector<double>> coefs, double start, double end)
+        {
+            Action action = () =>
+            {
+                chart.Series[1].Points.Clear();
+                chart.Series[2].Points.Clear();
+            };
+            Invoke(action);
+            string linearFuncString = "x*" + coefs[0][0].ToString() + "+" + coefs[0][1].ToString();
+            string sqrFuncString = "x^2*" + coefs[1][0].ToString() + "+x*" + coefs[1][1].ToString() + "+" + coefs[1][2].ToString();
+            Expression linearFun = Infix.ParseOrThrow(linearFuncString.Replace(',', '.'));
+            Expression sqrFunc = Infix.ParseOrThrow(sqrFuncString.Replace(',', '.'));
+
+            double step = (end - start) / 1000;
+            while (start < end)
+            {
+                action = () => {
+                    chart.Series[1].Points.AddXY(start, FuncValue(start, linearFun));
+                    chart.Series[2].Points.AddXY(start, FuncValue(start, sqrFunc));
+                };
+                Invoke(action);
+                start += step;
+            }
+        }
+
+
+
+        private double FuncValue(double point, Expression func)
+        {
+            Dictionary<string, FloatingPoint> symbol = new Dictionary<string, FloatingPoint>()
+            {
+                { "x", point }
+            };
+            return MathNet.Symbolics.Evaluate.Evaluate(symbol, func).RealValue;
+        }
+
+
+
+        private List<Vector<double>> CoefFinding(double[] xValue, double[] yValue)
+        {
+            double sumY = 0;
+            double sumX = 0;
+            double sumSqrX = 0;
+            double sumThrX = 0;
+            double sumQuadX = 0;
+            double sumXY = 0;
+            double sumSqrXY = 0;
+
+            for (int point = 0; point < xValue.Length; ++point)
+            {
+                sumY += yValue[point];
+                sumX += xValue[point];
+                sumSqrX += Math.Pow(xValue[point], 2);
+                sumThrX += Math.Pow(xValue[point], 3);
+                sumQuadX += Math.Pow(xValue[point], 4);
+                sumXY += xValue[point] * yValue[point];
+                sumSqrXY += Math.Pow(xValue[point], 2) * yValue[point];
+            }
+
+            Matrix<double> matrix = DenseMatrix.OfArray(new double[,] {
+                {sumQuadX, sumThrX, sumSqrX},
+                {sumThrX, sumSqrX, sumX},
+                {sumSqrX, sumX, xValue.Length - 1}});
+            Vector<double> answers = Vector<double>.Build.Dense(new double[] { sumSqrXY, sumXY, sumY });
+
+            Vector<double> sqrCoefs = matrix.SolveIterative(answers, new MlkBiCgStab());
+            Vector<double> linearCoefs = matrix.SubMatrix(1, 2, 1, 2).SolveIterative(answers.SubVector(1, 2), new MlkBiCgStab());
+
+            List<Vector<double>> coefsContainer = new List<Vector<double>> { linearCoefs, sqrCoefs };
+            return coefsContainer;
+        }
+        
+
+        private void CloseBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
